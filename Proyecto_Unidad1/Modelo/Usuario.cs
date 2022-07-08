77aea3ccdf4fd1581a764d487614cb28c5d3ac96
Viane <Viane@DESKTOP-S9OPB7O>
2022-07-08 15:08:06 -0500

    Agregar archivos de proyecto.

---
+++ Proyecto_Unidad1/Modelo/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Unidad1.Modelo
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+
+
+    }
+}
---
+++ Proyecto_Unidad1/Sistema.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Proyecto_Unidad1
+{
+    /// <summary>
+    /// Lógica de interacción para Sistema.xaml
+    /// </summary>
+    public partial class Sistema : Window
+    {
+        public Sistema()
+        {
+            InitializeComponent();
+        }
+    }
+}
