6caba2c0c1ddbd6f0d2001f704041bc1089dfc53
Viane <Viane@DESKTOP-S9OPB7O>
2022-07-08 15:08:06 -0500

    Proyecto.

---
+++ Proyecto_Unidad1/Context/AplicationdbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Proyecto_Unidad1.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Unidad1.Context
+{
+    public class AplicationdbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder option)
+        {
+            option.UseMySQL("server = localhost; database= proyecto_unidad1; user= root; password= ");
+        }
+        public DbSet<Usuario> Usuario { get; set; }
+
+    }
+}
---
+++ Proyecto_Unidad1/Migrations/20220629011530_example.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+namespace Proyecto_Unidad1.Migrations
+{
+    public partial class example : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Nombre = table.Column<string>(type: "text", nullable: false),
+                    User = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuario");
+        }
+    }
+}
